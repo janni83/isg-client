0bb69b9e56052fb4a59d7e784c01b39dbc56f381
Jan Stock <jan.stock83@gmail.com>
2018-08-03 23:35:25 +0200

    test for getting the capacity

--- test/cooling.test.js
+++ test/cooling.test.js
@@ -5,15 +5,18 @@ jest.mock('../lib/isg-client');
 
 describe('cooling', () => {
   it('fetches active cooling state from the cooling page', async () => {
-    expect.assertions(1);
     const instance = new CoolingModule(new IsgClientMock({ coolingActive: true }));
     const isActive = await instance.fetchIsActive();
     expect(isActive).toEqual(true);
   });
   it('fetches inactive cooling state from the cooling page', async () => {
-    expect.assertions(1);
     const instance = new CoolingModule(new IsgClientMock({ coolingActive: false }));
     const isActive = await instance.fetchIsActive();
     expect(isActive).toEqual(false);
+  });
+  it('fetches capacity', async () => {
+    const instance = new CoolingModule(new IsgClientMock({ coolingCapacity: 76 }));
+    const capacity = await instance.fetchCapacity();
+    expect(capacity).toEqual(76);
   });
 });
