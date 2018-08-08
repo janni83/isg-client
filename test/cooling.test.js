67c17137e5edc5341d877f7fafdd6d15a0bcfab2
Jan Stock <jan.stock83@gmail.com>
2018-08-08 21:57:07 +0200

    added heating module renamed some functions

--- test/cooling.test.js
+++ test/cooling.test.js
@@ -6,17 +6,17 @@ jest.mock('../lib/isg-client');
 describe('cooling', () => {
   it('fetches active cooling state from the cooling page', async () => {
     const instance = new CoolingModule(new IsgClientMock({ coolingActive: true }));
-    const isActive = await instance.fetchIsActive();
+    const isActive = await instance.isActive();
     expect(isActive).toEqual(true);
   });
   it('fetches inactive cooling state from the cooling page', async () => {
     const instance = new CoolingModule(new IsgClientMock({ coolingActive: false }));
-    const isActive = await instance.fetchIsActive();
+    const isActive = await instance.isActive();
     expect(isActive).toEqual(false);
   });
   it('fetches capacity of 76', async () => {
     const instance = new CoolingModule(new IsgClientMock({ coolingCapacity: 76 }));
-    const capacity = await instance.fetchCapacity();
+    const capacity = await instance.capacity();
     expect(capacity).toEqual(76);
   });
 });
